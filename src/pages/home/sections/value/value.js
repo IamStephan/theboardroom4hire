20a72d2193d56ce3e5b29af0a0267f49956a56e9
I_Am_Stephan <stephanburger54@gmail.com>
2020-02-27 05:35:20 +0200

    added animations to value section in home

--- src/pages/home/sections/value/value.js
+++ src/pages/home/sections/value/value.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react';
+import { Trail, Spring, config } from 'react-spring/renderprops';
 import { TextField, MenuItem, ThemeProvider, createMuiTheme, Button, InputAdornment } from '@material-ui/core';
 import { Email, CalendarToday, Person } from '@material-ui/icons';
 
@@ -34,135 +35,199 @@ export default class Value extends PureComponent {
   render() {
     return (
       <section className={styles['value']}>
-        <img alt='Hero Showing The Purpose' src={require('../../../../static/images/production/slide_1.jpg')} className={styles['value-img']} />
-        <div className={styles['left']}>
-          <h1 className={styles['value-header']}>
-            Giving you the floor for top class discussions
-          </h1>
-          <p className={styles['value-content']}>
-            Our business provides spacious meeting environments, that is well equipped with modern technology to provide the floor for top-class discussions.
-          </p>
-        </div>
-
-        <div className={styles['right']}>
-          <div className={styles['form-box']}>
-            <ThemeProvider theme={theme}>
-              <h3 className={styles['form-title']}>
-                Book Now
-              </h3>
-
-              <form
-                noValidate
-                onSubmit={this.handleSubmit}
-                className={styles['form']}
-              >
-                <TextField
-                  className={styles['item']}
-                  autoFocus
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position='start'>
-                        <Person />
-                      </InputAdornment>
-                    ),
-                  }}
-                  size='small'
-                  fullWidth
-                  variant='filled'
-                  color='secondary'
-                  label='Full Name'
-                />
-
-                <TextField
-                  className={styles['item']}
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position='start'>
-                        <Email />
-                      </InputAdornment>
-                    ),
-                  }}
-                  size='small'
-                  fullWidth
-                  variant='filled'
-                  color='secondary'
-                  label='Email'
-                />
-
-                <div className={styles['dual']}>
-                  <div className={styles['item']}>
-                    <TextField
-                      size='small'
-                      fullWidth
-                      select
-                      variant='filled'
-                      color='secondary'
-                      label='Package'
+        <ThemeProvider theme={theme}>
+          <img alt='Hero Showing The Purpose' src={require('../../../../static/images/production/slide_1.jpg')} className={styles['value-img']} />
+          <div className={styles['left']}>
+            <Trail
+              items={[
+                { type: 'h' },
+                { type: 's' }
+              ]}
+              keys={['h', 's']}
+              from={{
+                transform: 'translateX(-100%)',
+                opacity: 0
+              }}
+              to={{
+                transform: 'translateY(0)',
+                opacity: 1
+              }}
+              config={{
+                delay: 500
+              }}
+            >
+              {items => props => {
+                if(items.type === 'h') {
+                  return (
+                    <h1
+                      className={styles['value-header']}
+                      style={{
+                        transform: props.transform,
+                        opacity: props.opacity
+                      }}
                     >
-                      <MenuItem value='ph'>
-                        Per Hour
-                      </MenuItem>
-                      <MenuItem value='hd'>
-                        Half Day
-                      </MenuItem>
-                      <MenuItem value='fd'>
-                        Full Day
-                      </MenuItem>
-                    </TextField>
-                  </div>
-                  <div className={styles['item']}>
-                    <TextField
-                      size='small'
-                      fullWidth
-                      select
-                      variant='filled'
-                      color='secondary'
-                      label='People'
+                      Giving you the floor for top class discussions
+                    </h1>
+                  )
+                } else {
+                  return (
+                    <p
+                      className={styles['value-content']}
+                      style={{
+                        transform: props.transform,
+                        opacity: props.opacity
+                      }}
                     >
-                      <MenuItem value='sm'>
-                        1-4
-                      </MenuItem>
-                      <MenuItem value='md'>
-                        5-9
-                      </MenuItem>
-                      <MenuItem value='lg'>
-                        10-19
-                      </MenuItem>
-                      <MenuItem value='xl'>
-                        20-30
-                      </MenuItem>
-                    </TextField>
-                  </div>
-                </div>
-
-                <TextField
-                  InputProps={{
-                    startAdornment: (
-                      <InputAdornment position='start'>
-                        <CalendarToday />
-                      </InputAdornment>
-                    ),
-                  }}
-                  className={styles['item']}
-                  size='small'
-                  fullWidth
-                  variant='filled'
-                  color='secondary'
-                  label='Prefered Date'
-                />
-
-                <Button
-                  variant='outlined'
-                  color='default'
-                  type='submit'
-                  fullWidth
-                >
-                  Submit
-                </Button>
-              </form>
-            </ThemeProvider>
+                      Our business provides spacious meeting environments, that is well equipped with modern technology to provide the floor for top-class discussions.
+                    </p>
+                  )
+                }
+              }}
+            </Trail>
           </div>
-        </div>
+
+          <div className={styles['right']}>
+            <Spring
+              from={{
+                transform: 'scale(0.9)',
+                opacity: 0
+              }}
+              to={{
+                transform: 'scale(1)',
+                opacity: 1
+              }}
+
+              config={{
+                ...config.wobbly,
+                delay: 100
+              }}
+            >
+              {props => (
+                <div
+                  className={styles['form-box']}
+                  style={{
+                    transform: props.transform,
+                    opacity: props.opacity
+                  }}
+                >
+                  <h3 className={styles['form-title']}>
+                    Book Now
+                  </h3>
+
+                  <form
+                    noValidate
+                    onSubmit={this.handleSubmit}
+                    className={styles['form']}
+                  >
+                    <TextField
+                      className={styles['item']}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position='start'>
+                            <Person />
+                          </InputAdornment>
+                        ),
+                      }}
+                      size='small'
+                      fullWidth
+                      variant='filled'
+                      color='secondary'
+                      label='Full Name'
+                    />
+
+                    <TextField
+                      className={styles['item']}
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position='start'>
+                            <Email />
+                          </InputAdornment>
+                        ),
+                      }}
+                      size='small'
+                      fullWidth
+                      variant='filled'
+                      color='secondary'
+                      label='Email'
+                    />
+
+                    <div className={styles['dual']}>
+                      <div className={styles['item']}>
+                        <TextField
+                          size='small'
+                          fullWidth
+                          select
+                          variant='filled'
+                          color='secondary'
+                          label='Package'
+                        >
+                          <MenuItem value='ph'>
+                            Per Hour
+                          </MenuItem>
+                          <MenuItem value='hd'>
+                            Half Day
+                          </MenuItem>
+                          <MenuItem value='fd'>
+                            Full Day
+                          </MenuItem>
+                        </TextField>
+                      </div>
+                      <div className={styles['item']}>
+                        <TextField
+                          size='small'
+                          fullWidth
+                          select
+                          variant='filled'
+                          color='secondary'
+                          label='People'
+                        >
+                          <MenuItem value='sm'>
+                            1-4
+                          </MenuItem>
+                          <MenuItem value='md'>
+                            5-9
+                          </MenuItem>
+                          <MenuItem value='lg'>
+                            10-19
+                          </MenuItem>
+                          <MenuItem value='xl'>
+                            20-30
+                          </MenuItem>
+                        </TextField>
+                      </div>
+                    </div>
+
+                    <TextField
+                      InputProps={{
+                        startAdornment: (
+                          <InputAdornment position='start'>
+                            <CalendarToday />
+                          </InputAdornment>
+                        ),
+                      }}
+                      className={styles['item']}
+                      size='small'
+                      fullWidth
+                      variant='filled'
+                      color='secondary'
+                      label='Prefered Date'
+                    />
+
+                    <Button
+                      variant='outlined'
+                      color='default'
+                      type='submit'
+                      fullWidth
+                    >
+                      Submit
+                    </Button>
+                  </form>
+                </div>
+              )}
+            </Spring>
+
+          </div>
+        </ThemeProvider>
       </section>
     );
   }
